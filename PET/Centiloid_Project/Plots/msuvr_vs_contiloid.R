ac39e1f4e83a23b8dfe1a7cada603f34c1fad0f8
Ryn <kak240@musc.edu>
2025-10-20 11:14:26 -0400

    update r plots

--- PET/Centiloid_Project/Plots/msuvr_vs_contiloid.R
+++ PET/Centiloid_Project/Plots/msuvr_vs_contiloid.R
@@ -1,6 +1,6 @@
 library(ggplot2)
 
-df <- read.csv("/Volumes/vdrive/helpern_users/helpern_j/IAM/IAM_Analysis/IAM_BIDS/derivatives/pet/centiloid_project/Calibrated-Coefficients/centiloids_both_tracers.csv",
+df <- read.csv("/Users/kayti/Desktop/Projects/IAM/Centiloids/FBB_centiloids.csv",
                header = TRUE, stringsAsFactors = FALSE)
 
 # Combine tracer + amyloid to make a unique group
@@ -33,5 +33,5 @@ ggplot(df, aes(x = Centiloid, y = MeanSUVR, color = group)) +
     plot.title = element_text(hjust = 0.5)
   ) 
 
-ggsave("/Volumes/vdrive/helpern_users/helpern_j/IAM/IAM_Analysis/IAM_BIDS/derivatives/pet/centiloid_project/Calibrated-Coefficients/centiloid_vs_msuvr.pdf",
+ggsave("/Users/kayti/Desktop/Projects/IAM/Centiloids/centiloid_vs_msuvr.pdf",
        width = 8, height = 6)
